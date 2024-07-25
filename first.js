60eee354636ac5bcbd5fbfdb4b4ad4e0b0404672
Muthu kumar <mk2778555@gmail.com>
2024-07-25 23:48:56 +0530

    identifing zero

--- first.js
+++ first.js
@@ -1,8 +1,8 @@
-var arr=[342,342,324,243,243,432,432,324];
+var arr=[342,342,324,0,243,243,432,0,432,324];
 for (var i=1;i<=arr.length;i++)
-if (arr[i]>300){
+if (arr[i]===0){
 
-console.log("More then value");
+console.log("ZERO");
 
 }
 else if (arr[i]%2===0){
