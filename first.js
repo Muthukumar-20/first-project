4198381894b93ad3c497172bfc63c0590f2582f4
Muthu kumar <mk2778555@gmail.com>
2024-07-25 23:41:11 +0530

    updated

---
+++ first.js
@@ -0,0 +1,16 @@
+var arr=[342,342,324,243,243,432,432,324];
+for (var i=1;i<=arr.length;i++)
+if (arr[i]>300){
+
+console.log("More then value");
+
+}
+else if (arr[i]%2===0){
+console.log("EVEN");
+
+
+}
+else{
+
+    console.log("ODD");
+}
